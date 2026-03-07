943f2256b0fbd4cb2148a0566eb603166be53047
Benjamin <benjamin.stoica>
2026-03-07 11:36:55 +0100

    fix(ui): estendi il materiale originale a tutta la sidebar

--- Sources/CoderIDE/Modules/App/Sidebar/SidebarView.swift
+++ Sources/CoderIDE/Modules/App/Sidebar/SidebarView.swift
@@ -33,13 +33,13 @@ struct SidebarView: View {
 
     let checkpointGitStore = ConversationCheckpointGitStore()
 
-    var sidebarBackgroundColor: Color {
-        DesignSystem.Colors.chatPanelSolidBackground
-    }
-
     var body: some View {
-        sidebarContent
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .background(sidebarBackgroundColor)
+        ZStack {
+            SidebarMaterialBackground()
+                .ignoresSafeArea()
+
+            sidebarContent
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
